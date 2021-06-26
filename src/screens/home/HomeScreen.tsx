234c8be99e4a8e1fa8a7983befef68363ca1bb6c
Rahmah Mahmoud <rahmah.mahmoud@sprintward.com>
2021-06-26 08:08:55 +0300

    some UI in home screen

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -4,25 +4,30 @@ import Header from 'SwensonheTask/src/components/Header';
 import Heart from 'SwensonheTask/assets/icons/heart.png'
 import IconImage from 'SwensonheTask/src/components/IconImage';
 import Title from 'SwensonheTask/src/components/Title';
+import { Colors } from 'SwensonheTask/assets/styles/Colors';
 
 export default function HomeScreen() {
 
   return (
     <>
       <Header />
-      <View style={{ backgroundColor: 'green', flex: 1 }}>
-        <View style={{ backgroundColor: 'red', flex: .6, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 50, }}>
-          <View style={{ backgroundColor: 'gold', }}>
-            <IconImage source={Heart} />
-          </View>
-          <View style={{ backgroundColor: 'pink', paddingHorizontal: 10 }}>
-            <Text>nnnnnn<Text>dddddd</Text></Text>
-          </View>
-          <View style={{ backgroundColor: 'purple', }}></View>
-          <Title title='65' />
+      <View style={{ backgroundColor: 'green', flex: 1 ,}}>
+        <View style={{ backgroundColor: 'red', flex: .6, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 50,justifyContent:'center' }}>
+            <IconImage source={Heart} small />
+            <Text style={{marginHorizontal:10, marginLeft:20,fontSize:50,fontWeight:'bold'}}>65</Text>
+          <Text style={{ marginBottom: 20 }}>BPM</Text>
+
+        
 
         </View>
+        
         <View style={{ backgroundColor: 'blue', flex: 1 }}>
+          <View style={{ backgroundColor: 'purple', flexDirection: 'row', padding: 20}}>
+            <Text style={{color:Colors.BLACK}}>LED 1</Text>
+            <Text style={{ color: Colors.LIGHT_GRAY ,marginHorizontal:20}}>LED 2</Text>
+            <Text style={{ color: Colors.LIGHT_GRAY }}>LED 3</Text>
+
+          </View>
 
         </View>
       </View>

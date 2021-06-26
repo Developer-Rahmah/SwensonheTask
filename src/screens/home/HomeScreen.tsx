a7584b9f2c371ab45c7d9a3d511ddff65f690256
Rahmah Mahmoud <rahmah.mahmoud@sprintward.com>
2021-06-26 09:55:50 +0300

    adding chart

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,11 +1,36 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, Animated } from 'react-native';
+import { View, Text, Animated,Dimensions } from 'react-native';
 import Header from 'SwensonheTask/src/components/Header';
 import Heart from 'SwensonheTask/assets/icons/heart.png'
 import IconImage from 'SwensonheTask/src/components/IconImage';
 import Title from 'SwensonheTask/src/components/Title';
 import { Colors } from 'SwensonheTask/assets/styles/Colors';
 import { Ionicons } from '@expo/vector-icons';
+//@ts-ignore
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+  monotoneCubicInterpolation,
+} from '@rainbow-me/animated-charts';
+
+export const { width: SIZE } = Dimensions.get('window');
+
+export const data = [
+  { x: 1453075200, y: 1.47 }, { x: 1453161600, y: 2.37 },
+  { x: 1453248000, y: 1.53 }, { x: 1453334400, y: .54 },
+  { x: 1453420800, y: 1.52 }, { x: 1453507200, y: 1.03 },
+  { x: 1453593600, y: 2.10 }, { x: 1453680000, y: 1.50 },
+  { x: 1453593600, y: 2.12 }, { x: 1453680000, y: 1.50 },
+  { x: 1453593600, y: 2.11 }, { x: 1453680000, y: 1.53 },
+  { x: 1453593600, y: 2.10 }, { x: 1453680000, y: 1.55 },
+
+  { x: 1453766400, y: 2.30 }, { x: 1453852800, y: 1.42 },
+  { x: 1453939200, y: 2.55 }, { x: 1454025600, y: 1.41 },
+  { x: 1454112000, y: 2.43 }, { x: 1454198400, y: 1.20 },
+];
+
+const points = monotoneCubicInterpolation({ data, range: 40 });
 
 export default function HomeScreen() {
   const anim = useRef(new Animated.Value(1));
@@ -48,7 +73,7 @@ export default function HomeScreen() {
   return (
     <>
       <Header />
-      <View style={{ backgroundColor: 'green', flex: 1 ,}}>
+      <View style={{ flex: 1 ,}}>
         <View style={{  flex: .6, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 50,justifyContent:'center' }}>
             {/* <IconImage source={Heart} small /> */}
           <Animated.View style={{ transform: [{ scale: anim.current }] }}>
@@ -61,8 +86,25 @@ export default function HomeScreen() {
 
         </View>
         
-        <View style={{ backgroundColor: 'blue', flex: 1 }}>
-          <View style={{ backgroundColor: 'purple', flexDirection: 'row', padding: 20}}>
+        <View style={{  flex: 1 }}>
+          <View
+            style={{
+              backgroundColor: 'red',
+            }}>
+            <ChartPathProvider
+              data={{
+                points,
+                smoothingStrategy: 'bezier',
+              }}>
+              <ChartPath height={SIZE / 2} stroke="yellow" width={SIZE} />
+              <ChartDot
+                style={{
+                  backgroundColor: 'blue',
+                }}
+              />
+            </ChartPathProvider>
+          </View>
+          <View style={{  flexDirection: 'row', padding: 20}}>
             <Text style={{color:Colors.BLACK}}>LED 1</Text>
             <Text style={{ color: Colors.LIGHT_GRAY ,marginHorizontal:20}}>LED 2</Text>
             <Text style={{ color: Colors.LIGHT_GRAY }}>LED 3</Text>
@@ -74,3 +116,5 @@ export default function HomeScreen() {
     </>
   );
 }
+
+
